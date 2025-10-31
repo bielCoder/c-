30746006960605e7d018779162c7c17f08c82784
bielCoder <gabrielhenrique9253@gmail.com>
2025-10-31 10:21:34 -0300

    Feat: aprendendo declaração de variáveis e seus tipos

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaração de variáveis
+            int number = 10;
+            char letter = 'A';
+            double dollars = 99.99;
+            string message = "Hello";
+            string name = "John Doe";
+            bool isActive = true;
+
+            Console.WriteLine("Integer: " + number);
+            Console.WriteLine("Character: " + letter);
+            Console.WriteLine("Double: " + dollars);
+            Console.WriteLine(message + ", " + name + "!");
+            
+        }
+    }
+}
