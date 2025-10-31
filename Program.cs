f66a2e65ee2d8d325405e5b8b4e1177d14c878f0
bielCoder <gabrielhenrique9253@gmail.com>
2025-10-31 10:26:34 -0300

    Feat: Operadores Aritmeticos

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aula04
+{
+    class Program
+    {
+        // Operadores Aritméticos na prática
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 3;
+
+            Console.WriteLine("Soma: " + (a + b));          // Adição
+            Console.WriteLine("Subtração: " + (a - b));     // Subtração
+            Console.WriteLine("Multiplicação: " + (a * b));  // Multiplicação
+            Console.WriteLine("Divisão: " + (a / b));        // Divisão
+            Console.WriteLine("Módulo: " + (a % b));         // Módulo
+        }
+    }
+}
