4926fd12d231b64c6b9cb82861cc43aaeb66cb5f
bielCoder <gabrielhenrique9253@gmail.com>
2025-11-03 15:18:16 -0300

    Feat: typecasting implicito e explicito

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace aula11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aprendizado sobre typecasting em C#
+            int n1 = 10;
+            float n2 = n1;
+
+            Console.WriteLine("Valor de n2 após typecasting implícito: " + n2);
+
+            // Typecasting explícito
+            float n3 = 9.7f;
+            int n4 = (int)n3;
+            Console.WriteLine("Valor de n4 após typecasting explícito: " + n4);
+        }
+    }
+}
