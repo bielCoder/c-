273b6ebb9cbda5bedfbbad8281900df6595b74e9
bielCoder <gabrielhenrique9253@gmail.com>
2025-11-03 14:59:14 -0300

    Feat: aprendendo sobre closure enum

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Aula10
+{
+    class Program
+    {
+        enum WeekDays { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+
+        static void Main(string[] args)
+        {
+            int today = (int) WeekDays.Monday;
+            Console.WriteLine("Today is: " + today);
+        }
+    }
+}
