6f907dce3520144828b22f1a9f1c68079e0ab254
bielCoder <gabrielhenrique9253@gmail.com>
2025-11-03 13:30:53 -0300

    Feat: aprendendo a função de uma constante e qual sua finalidade

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace aula07
+{
+    class Program
+    {
+        public const string fullname = "Gabriel Henrique";
+        
+        
+
+        static void Main(string[] args)
+        {
+            // uso de constantes
+            this.fullname = "João Silva"; // Erro: não é possível modificar uma constante
+            Console.WriteLine("Nome completo: " + fullname);
+        }
+    }
+}
