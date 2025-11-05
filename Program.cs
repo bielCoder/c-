5f794f63caa758120679da1a31114b1a8e153c2c
bielCoder <gabrielhenrique9253@gmail.com>
2025-11-05 11:52:37 -0300

    Feat: aula sobre vetores e arrays

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros = [1, 2, 3, 4, 5];
+            Console.WriteLine(typeof(numeros));
+        }
+    }
+}
