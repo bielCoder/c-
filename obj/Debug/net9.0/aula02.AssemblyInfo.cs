0cb51961b32f811d403123d56199cacc7cc74f8a
bielCoder <gabrielhenrique9253@gmail.com>
2025-11-07 11:31:52 -0300

    ajuste de assebly

--- obj/Debug/net9.0/aula02.AssemblyInfo.cs
+++ obj/Debug/net9.0/aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ddfcf8e046f2a83854d9ab02054b73952b60bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3552d84eeedfcbd09668344408798147f2db5f62")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
