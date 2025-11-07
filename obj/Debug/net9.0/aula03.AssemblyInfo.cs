efcb1b21e11ed988d64ab615f5b4c42d3ceaf8cc
bielCoder <gabrielhenrique9253@gmail.com>
2025-11-07 11:32:42 -0300

    Feat: ajuste de assembly

--- obj/Debug/net9.0/aula03.AssemblyInfo.cs
+++ obj/Debug/net9.0/aula03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4a628f181c72c3c0f9bf53fd4f0948c0de12c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d31b9de37d90495eef746cc959b1a50c7d75c19d")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
