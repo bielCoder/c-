dbba17e919cb6b659a6092df052ed493177d7191
bielCoder <gabrielhenrique9253@gmail.com>
2025-11-11 12:16:43 -0300

    Feat: aprendendo sobre interfaces

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public interface Veiculo
+{
+    bool On();
+    bool Off();
+
+    void info()
+    {
+        Console.WriteLine("Informações do veículo.");
+    }
+}
+
+class VeiculoBase : Veiculo
+{
+    protected bool powerStatus;
+
+    public VeiculoBase()
+    {
+        powerStatus = false;
+    }
+
+    public bool On()
+    {
+        powerStatus = true;
+        Console.WriteLine("Veículo ligado.");
+        return powerStatus;
+    }
+
+    public bool Off()
+    {
+        powerStatus = false;
+        Console.WriteLine("Veículo desligado.");
+        return powerStatus;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Veiculo meuVeiculo = new VeiculoBase();
+        meuVeiculo.On();  
+        meuVeiculo.Off();
+    }
+}
