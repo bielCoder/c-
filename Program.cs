c20a922ff0540d2b5d0d8bb8aaa19148fc5d31a2
bielCoder <gabrielhenrique9253@gmail.com>
2025-12-01 16:01:31 -0300

    Feat: aula de sobrecarga

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+class Calculator
+{
+    public int sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calculator = new Calculator();
+        int result; // ← DECLAREI AQUI
+
+        do
+        {
+            Console.WriteLine("Enter the first number:");
+            int firstnumber = int.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter the second number:");
+            int secondnumber = int.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+            result = calculator.sum(firstnumber, secondnumber);
+
+        } while (result != 12);  // ← TERMINA AQUI (SEM OUTRO BLOCO)
+
+        Console.WriteLine("You found the secret number 12!");
+        Console.WriteLine("The sum is: " + result);
+    }
+}
