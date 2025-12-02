7ed58baa35d509a7c1f23748a8b4ae3838bc8318
bielCoder <gabrielhenrique9253@gmail.com>
2025-12-02 12:50:41 -0300

    Feat: aula sobre métodos e variáveis estáticas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Aula21
+{
+    class Mat
+    {
+        public static double pi = 3.14;
+
+        public static int dobro(int n)
+        {
+            return n * 2;
+        }
+    }
+    class Program
+    {
+
+        // ✔️ Use static quando:
+
+        // O comportamento não depende de valores diferentes por objeto.
+
+        // O valor é global.
+
+        // A lógica é pura, sem depender de estado interno.
+
+        // ❌ NÃO use static quando:
+
+        // Cada objeto precisa ter seu próprio estado.
+
+        // O método depende de variáveis internas do objeto.
+
+        static void Main(string[] args)
+        {
+            // Métodos e Variáveis Estáticas
+
+            double vpi = Mat.pi;
+            int num = 10;
+
+            Console.WriteLine("O dobro de " + num + " é " + Mat.dobro(num));
+        }
+    }
+}
