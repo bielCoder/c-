479cc059170bfe311a2ffbdf882a07df38678547
bielCoder <gabrielhenrique9253@gmail.com>
2025-12-02 15:29:46 -0300

    Feat: aprendendo sobre exceçoes try,catch e finally

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Aula24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aula sobre tratamento de exceções
+            try
+            {
+                Console.WriteLine("Digite um número inteiro:");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Você digitou o número: {numero}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erro: Formato inválido. Por favor, digite um número inteiro.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro inesperado: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Obrigado por usar o programa. (Idependentemente de erros ou sucessos, esta mensagem sempre será exibida. )");
+            }
+        }
+    }
+}
