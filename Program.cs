6c0b692a95dd527903fd1db04df6c5a11ab6470f
bielCoder <gabrielhenrique9253@gmail.com>
2025-12-02 15:15:07 -0300

    Feat: Estudando a passagem de argumentos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Aula23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aula sobre argumentos passados via terminal
+            Console.WriteLine("A quantidade de argumentos é: " + args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Argumento {i + 1}: {args[i]}");
+                Console.WriteLine("A soma dos argumentos numéricos é: " + (int.Parse(args[i]) + int.Parse(args[i])) );
+
+            }
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"Hello,{args[0]}" );
+        }
+    }
+}
