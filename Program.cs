557a6f2c4e9513c12cd3cc64978501362a60c0f7
bielCoder <gabrielhenrique9253@gmail.com>
2025-12-02 12:30:40 -0300

    Feat: aula sobre recursividade

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+class Calculator
+{
+    public int sum(int a, int b) => a + b;
+    public int sum(int a, int b, int c) => a + b + c;
+
+    public int factorial(int n)
+    {
+        if (n <= 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int res;
+        Calculator calculator = new Calculator();
+
+        res = calculator.factorial(10);
+        Console.WriteLine("Factorial of 5 is: " + res);
+        // int result = ConditionRepeat(calculator); // ← agora recebe um valor
+
+        // Console.WriteLine("You found the secret number 12!");
+        // Console.WriteLine("The sum is: " + result);
+
+
+
+    }
+
+    // static int ConditionRepeat(Calculator calculator)
+    // {
+    //     int result;
+    //     do
+    //     {
+    //         Console.WriteLine("Enter the first number:");
+    //         int firstnumber = int.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+    //         Console.WriteLine("Enter the second number:");
+    //         int secondnumber = int.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+    //         result = calculator.sum(firstnumber, secondnumber);
+
+    //     } while (result != 12);
+
+    //     return result; // ← devolve para o Main
+    // }
+
+  
+}
