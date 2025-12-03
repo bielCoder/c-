52662f363150e20b49a002ceef0589e07442a3a1
bielCoder <gabrielhenrique9253@gmail.com>
2025-12-03 13:12:21 -0300

    Feat: aprendendo sobre List

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> vehicles = new List<string> { "Carro", "Avião", "Barco" };
+        List<string> moreVehicles = new List<string> { "Bicicleta", "Moto" };
+
+        vehicles.AddRange(moreVehicles);
+
+        vehicles.Insert(1, "Patins");
+        vehicles.Sort();
+
+        // ------ EXEMPLO DE CopyTo ------
+        // Criando um array com o mesmo tamanho da lista
+        string[] vehiclesArray = new string[8];
+
+        // Copiando os elementos da lista para o array
+        vehicles.CopyTo(vehiclesArray,1);
+
+        Console.WriteLine("===== LISTA ORDENADA =====");
+        foreach (string vehicle in vehicles)
+        {
+            Console.WriteLine(vehicle);
+        }
+
+        Console.WriteLine("\n===== ARRAY COPIADO (CopyTo) =====");
+        foreach (string v in vehiclesArray)
+        {
+            Console.WriteLine(v);
+        }
+        Console.WriteLine("\n===== BUSCA POR ELEMENTO =====");
+        Console.WriteLine("Search for 'vehicle'");
+        string? find = vehicles.Find(x => x == "Barco");
+        Console.WriteLine("Found: " + find);
+        Console.WriteLine("\n===== BUSCA POR POSICIONAMENTO ELEMENTO =====");
+        Console.WriteLine("index of 'vehicle'");
+        int pos = vehicles.IndexOf("Barco");
+        Console.WriteLine("Found at position: " + pos);
+    }
+}
