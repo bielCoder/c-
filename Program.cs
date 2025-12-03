72f390a77020a460e677066d44402a6e44e067d7
bielCoder <gabrielhenrique9253@gmail.com>
2025-12-03 10:57:20 -0300

    Feat: aprendendo sobre namespaces

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Areas
+{
+    class Area
+    {
+        public static float Square(float baseLength, float altura)
+        {
+            if (baseLength == 0 || altura == 0)
+            {
+                throw new ArgumentException("Base ou altura não podem ser zero.");
+            }
+
+            return baseLength * altura;
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        try
+        {
+            float area = Areas.Area.Square(0, 5);
+            Console.WriteLine($"Área do quadrado.:{0} {area}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}.:{0}");
+        } finally
+        {
+            Console.WriteLine("Execução finalizada.");
+        }   
+    }
+}
