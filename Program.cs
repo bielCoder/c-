03c55efd6031606abcc120f37f5800f49166099a
bielCoder <gabrielhenrique9253@gmail.com>
2025-12-03 11:45:27 -0300

    Feat: Aprendendo sobre LinkedList

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Aula27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try {
+            
+                LinkedList<string> transport = new LinkedList<string>();
+                transport.AddLast("Carro");
+                transport.AddFirst("Avião");
+                transport.AddAfter(transport.First!, "Bicicleta");
+                transport.AddBefore(transport.Last!, "Barco");
+                
+                foreach (var item in transport)
+                {
+                    Console.WriteLine(item);
+                }
+            } catch (Exception ex) {
+                Console.WriteLine($"Erro: {ex.Message}");   
+        } finally {
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Programa finalizado.");
+           }
+        }
+    }
+}
