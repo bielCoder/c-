9f8966517293862e6c10d4cde0b35a1ebb9bc0bf
bielCoder <gabrielhenrique9253@gmail.com>
2025-12-03 10:37:47 -0300

    removendo arquivo de build do git

--- obj/Debug/net9.0/aula08.AssemblyInfo.cs
+++ obj/Debug/net9.0/aula08.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9c4d57fe4984caa311f1b620d405b508cc645e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+621a353bc48e1696acce2ad356fb91b652131b6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula08")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
